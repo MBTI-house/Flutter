7feae7550efe14ff63555c233e8b41f4db21128a
bluewow <dogseen@gmail.com>
2022-10-10 01:30:58 +0900

    feat: 로그인 방식 변경

--- lib/screen/kakao_login_screen.dart
+++ lib/screen/kakao_login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 class KakaoLoginScreen extends StatefulWidget {
@@ -9,6 +10,7 @@ class KakaoLoginScreen extends StatefulWidget {
   State<KakaoLoginScreen> createState() => _KakaoLoginScreenState();
 }
 
+//TODO loginWithKakaoTalk() 기기에 카톡이 설치되있는 경우
 class _KakaoLoginScreenState extends State<KakaoLoginScreen> {
   @override
   Widget build(BuildContext context) {
@@ -31,9 +33,36 @@ class _KakaoLoginScreenState extends State<KakaoLoginScreen> {
   }
 
   Future<void> _loginButtonPressed() async {
-    String authCode = await AuthCodeClient.instance.request();
-    print('----');
-    print(authCode);
-    print('----');
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+      }
+    } else {
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+
+    try {
+      User user = await UserApi.instance.me();
+      print('사용자 정보 요청 성공'
+          '\n회원번호: ${user.id}'
+          '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
+          '\n이메일: ${user.kakaoAccount?.email}');
+    } catch (error) {
+      print('사용자 정보 요청 실패 $error');
+    }
   }
 }
